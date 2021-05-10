9cbb936b86ab04523aa255e12cd8ef99e2428b9b
Nathaniel Tjandra <ntjandra@ucsc.edu>
2021-05-10 11:01:39 -0700

    Configured to connect and edit localhost postgres db

--- api/api/settings.py
+++ api/api/settings.py
@@ -77,13 +77,18 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Configure to use Postgres and connect to our server
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'carna',
+        'USER': 'api',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
