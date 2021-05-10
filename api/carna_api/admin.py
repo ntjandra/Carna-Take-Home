8b9b4ca1d051085d7b577abae2d14e7c5107d485
Nathaniel Tjandra <ntjandra@ucsc.edu>
2021-05-10 09:40:04 -0700

    Ignored Django files, track migrations

--- api/carna_api/admin.py
+++ api/carna_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Course
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+# Register your models here.
+
+admin.site.register(Course, CourseAdmin)
--- api/carna_api/models.py
+++ api/carna_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for Course 
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
