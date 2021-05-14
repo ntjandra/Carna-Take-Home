1e1af370038bf38b560dd57ce40c21beb2b9900e
Nathaniel Tjandra <ntjandra@ucsc.edu>
2021-05-14 10:48:53 -0700

    Add Import/Export to User and Group model.

--- api/carna_api/admin.py
+++ api/carna_api/admin.py
@@ -1,10 +1,16 @@
 from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 # from django.contrib.auth.admin import UserAdmin
 from .models import Course #, Questions, CarnaUser
 
 
-# Site Admins can manage Users and Courses.
+# Site Admins can manage Users, Courses, and Groups.
+class UserAdmin(ImportExportModelAdmin):
+    pass
+
+class GroupAdmin(ImportExportModelAdmin):
+    pass
 
 # Users have first_name, last_name, bio
 # class UserAdmin(admin.ModelAdmin):
@@ -16,3 +22,8 @@ class CourseAdmin(ImportExportModelAdmin):
 
 # admin.site.register(CarnaUser, UserAdmin)
 admin.site.register(Course, CourseAdmin)
+# Need to unreqister then register for built-in models, User and Groups.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
