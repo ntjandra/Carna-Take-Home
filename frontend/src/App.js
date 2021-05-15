86099bdb9fa38668cb7cb3b89f2399414618925c
Nathaniel Tjandra <ntjandra@ucsc.edu>
2021-05-15 12:42:02 -0700

    Version Dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
 import { Container } from "reactstrap";
